c5e1e48d722ff514da2ede2ea6e7678b7356f704
ESTHEREKANEM <estekanem1@gmail.com>
2023-07-20 18:29:49 +0000

    Inclusion

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -3,47 +3,45 @@
     A script that reads stdin line by line and computes metrics
 """
 import sys
+import signal
 
+def print_statistics(file_sizes, status_codes):
+    total_size = sum(file_sizes)
+    print(f"Total file size: File size: {total_size}")
 
-def print_msg(codes, file_size):
-    print("File size: {}".format(file_size))
-    for key, val in sorted(codes.items()):
-        if val != 0:
-            print("{}: {}".format(key, val))
+    for status_code in sorted(status_codes):
+        if status_code in valid_status_codes:
+            print(f"{status_code}: {status_codes[status_code]}")
 
+def parse_line(line):
+    parts = line.split()
+    if len(parts) != 10 or not parts[6].isdigit():
+        return None, None
 
-file_size = 0
-code = 0
-count_lines = 0
-codes = {
-    "200": 0,
-    "301": 0,
-    "400": 0,
-    "401": 0,
-    "403": 0,
-    "404": 0,
-    "405": 0,
-    "500": 0
-}
+    ip_address = parts[0]
+    status_code = int(parts[8])
+    file_size = int(parts[9])
+
+    return status_code, file_size
+
+valid_status_codes = {200, 301, 400, 401, 403, 404, 405, 500}
+file_sizes = []
+status_codes = {}
 
 try:
+    line_count = 0
     for line in sys.stdin:
-        parsed_line = line.split()
-        parsed_line = parsed_line[::-1]
+        status_code, file_size = parse_line(line.strip())
+        if status_code is None:
+            continue
 
-        if len(parsed_line) > 2:
-            count_lines += 1
+        file_sizes.append(file_size)
+        status_codes[status_code] = status_codes.get(status_code, 0) + 1
 
-            if count_lines <= 10:
-                file_size += int(parsed_line[0])
-                code = parsed_line[1]
+        line_count += 1
+        if line_count % 10 == 0:
+            print_statistics(file_sizes, status_codes)
 
-                if (code in codes.keys()):
-                    codes[code] += 1
-
-            if (count_lines == 10):
-                print_msg(codes, file_size)
-                count_lines = 0
-
-finally:
-    print_msg(codes, file_size)
+except KeyboardInterrupt:
+    print_statistics(file_sizes, status_codes)
+    sys.exit(0)

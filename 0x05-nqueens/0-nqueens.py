90cf376710264bf08a308375d814a72fe2d2829a
ESTHEREKANEM <estekanem1@gmail.com>
2023-08-03 21:24:11 +0000

    readme update

---
+++ 0x05-nqueens/0-nqueens.py
@@ -0,0 +1,58 @@
+ import sys
+
+def is_safe(board, row, col, N):
+    for i in range(row):
+        if board[i][col] == 'Q':
+            return False
+    
+    for i, j in zip(range(row, -1, -1), range(col, -1, -1)):
+        if board[i][j] == 'Q':
+            return False
+    
+    for i, j in zip(range(row, -1, -1), range(col, N)):
+        if board[i][j] == 'Q':
+            return False
+    
+    return True
+
+def solve_nqueens(N):
+    board = [['.' for _ in range(N)] for _ in range(N)]
+    solutions = []
+
+    def backtrack(row):
+        if row == N:
+            solution = [''.join(row) for row in board]
+            solutions.append(solution)
+            return
+
+        for col in range(N):
+            if is_safe(board, row, col, N):
+                board[row][col] = 'Q'
+                backtrack(row + 1)
+                board[row][col] = '.'
+
+    backtrack(0)
+    return solutions
+
+def print_solutions(solutions):
+    for solution in solutions:
+        print('\n'.join(solution))
+        print()
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: nqueens N")
+        sys.exit(1)
+
+    try:
+        N = int(sys.argv[1])
+        if N < 4:
+            print("N must be at least 4")
+            sys.exit(1)
+
+        solutions = solve_nqueens(N)
+        print_solutions(solutions)
+
+    except ValueError:
+        print("N must be a number")
+        sys.exit(1)

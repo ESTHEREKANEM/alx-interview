e54dbd80420cd3f1feeb347c1851510e5eaa86d7
ESTHEREKANEM <estekanem1@gmail.com>
2023-08-03 21:33:55 +0000

    Nqueen pdate

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -1,58 +1,51 @@
- import sys
+#!/usr/bin/python3
 
-def is_safe(board, row, col, N):
-    for i in range(row):
-        if board[i][col] == 'Q':
-            return False
-    
-    for i, j in zip(range(row, -1, -1), range(col, -1, -1)):
-        if board[i][j] == 'Q':
-            return False
-    
-    for i, j in zip(range(row, -1, -1), range(col, N)):
-        if board[i][j] == 'Q':
-            return False
-    
-    return True
+import sys
 
-def solve_nqueens(N):
-    board = [['.' for _ in range(N)] for _ in range(N)]
-    solutions = []
 
-    def backtrack(row):
-        if row == N:
-            solution = [''.join(row) for row in board]
-            solutions.append(solution)
-            return
+def solve(row, column):
+    solver = [[]]
+    for q in range(row):
+        solver = place_queen(q, column, solver)
+    return solver
 
-        for col in range(N):
-            if is_safe(board, row, col, N):
-                board[row][col] = 'Q'
-                backtrack(row + 1)
-                board[row][col] = '.'
 
-    backtrack(0)
-    return solutions
+def place_queen(q, column, prev_solver):
+    return [array + [x] for array in prev_solver for x in range(column) if is_safe(q, x, array)]
 
-def print_solutions(solutions):
-    for solution in solutions:
-        print('\n'.join(solution))
-        print()
 
-if __name__ == "__main__":
+def is_safe(q, x, array):
+    if x in array:
+        return False
+    else:
+        return all(abs(array[column] - x) != q - column for column in range(q))
+
+
+def init():
     if len(sys.argv) != 2:
         print("Usage: nqueens N")
         sys.exit(1)
-
-    try:
-        N = int(sys.argv[1])
-        if N < 4:
-            print("N must be at least 4")
-            sys.exit(1)
-
-        solutions = solve_nqueens(N)
-        print_solutions(solutions)
-
-    except ValueError:
+    if sys.argv[1].isdigit():
+        the_queen = int(sys.argv[1])
+    else:
         print("N must be a number")
         sys.exit(1)
+    if the_queen < 4:
+        print("N must be at least 4")
+        sys.exit(1)
+    return the_queen
+
+
+def n_queens():
+    the_queen = init()
+    solver = solve(the_queen, the_queen)
+    for array in solver:
+        clean = []
+        for q, x in enumerate(array):
+            clean.append([q, x])
+        print(clean)
+
+
+if __name__ == '__main__':
+    n_queens()
+

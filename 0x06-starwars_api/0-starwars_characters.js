f61079931f67923f33eb8072c40fd28f010dbfaf
ESTHEREKANEM <estekanem1@gmail.com>
2023-08-10 20:52:18 +0000

    star wars characters and readme update

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -2,28 +2,22 @@
 
 const request = require('request');
 const url = `https://swapi-api.alx-tools.com/api/films/${process.argv[2]}/`;
-const characters = [];
-
 request(url, async function (error, response, body) {
   if (error) {
     return console.log(error);
   } else {
-    const characterData = JSON.parse(body).characters;
-    characters.push(...characterData);
+    const characters = JSON.parse(body).characters;
+    for (const character in characters) {
+      const res = await new Promise((resolve, reject) => {
+        request(characters[character], (err, res, html) => {
+          if (err) {
+            reject(err);
+          } else {
+            resolve(JSON.parse(html).name);
+          }
+        });
+      });
+      console.log(res);
+    }
   }
 });
-
-let i = 0;
-while (i < characters.length) {
-  const res = await new Promise((resolve, reject) => {
-    request(characters[i], (err, res, html) => {
-      if (err) {
-        reject(err);
-      } else {
-        resolve(JSON.parse(html).name);
-      }
-    });
-  });
-  console.log(res);
-  i++;
-}

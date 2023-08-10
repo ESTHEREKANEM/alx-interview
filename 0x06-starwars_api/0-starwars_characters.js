6af7b819550e53969a0a273d272eb128075a46a4
ESTHEREKANEM <estekanem1@gmail.com>
2023-08-10 20:49:52 +0000

    star wars characters and readme update

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,37 +1,29 @@
 #!/usr/bin/node
+
 const request = require('request');
+const url = `https://swapi-api.alx-tools.com/api/films/${process.argv[2]}/`;
+const characters = [];
 
-function getMovieCharacters(movieId) {
-    const filmUrl = `https://swapi.dev/api/films/${movieId}/`;
+request(url, async function (error, response, body) {
+  if (error) {
+    return console.log(error);
+  } else {
+    const characterData = JSON.parse(body).characters;
+    characters.push(...characterData);
+  }
+});
 
-    request.get(filmUrl, (error, response, body) => {
-        if (error) {
-            console.log(`Error fetching film data for Movie ID ${movieId}`);
-            return;
-        }
-
-        const filmData = JSON.parse(body);
-        const characters = filmData.characters;
-
-        console.log(`Characters in ${filmData.title} (Episode ${filmData.episode_id}):`);
-        characters.forEach(characterUrl => {
-            request.get(characterUrl, (characterError, characterResponse, characterBody) => {
-                if (characterError) {
-                    console.log(`Error fetching character data for ${characterUrl}`);
-                    return;
-                }
-
-                const characterData = JSON.parse(characterBody);
-                console.log(characterData.name);
-            });
-        });
+let i = 0;
+while (i < characters.length) {
+  const res = await new Promise((resolve, reject) => {
+    request(characters[i], (err, res, html) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(JSON.parse(html).name);
+      }
     });
+  });
+  console.log(res);
+  i++;
 }
-
-if (process.argv.length !== 3) {
-    console.log("Usage: node script.js <Movie ID>");
-    process.exit(1);
-}
-
-const movieId = process.argv[2];
-getMovieCharacters(movieId);

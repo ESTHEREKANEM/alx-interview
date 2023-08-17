235725a12c3d17eac78f167e8e5954e687bd363b
ESTHEREKANEM <estekanem1@gmail.com>
2023-08-17 16:45:20 +0000

    rotate matrix

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -5,14 +5,22 @@ def rotate_2d_matrix(matrix):
     Args:
         matrix
     """
-    for layer in range(len(matrix) // 2):
-        first = layer
-        last = len(matrix) - layer - 1
-        for i in range(last - first):
-            temp = matrix[first][first + i]
-            matrix[first][first + i] = matrix[last - i][first]
-            matrix[last - i][first] = matrix[last][last - i]
-            matrix[last][last - i] = matrix[first + i][last]
-            matrix[first + i][last] = temp
+    left, right = 0, len(matrix) - 1
 
-
+    while left < right:
+        i = 0
+        while i < right - left:
+            top, bottom = left, right
+            # save topleft  value
+            topLeft = matrix[top][left + i]
+            # move bottom left to top left
+            matrix[top][left + i] = matrix[bottom - i][left]
+            # move bottom right to bottom left
+            matrix[bottom - i][left] = matrix[bottom][right - i]
+            # move top right to bottom right
+            matrix[bottom][right - i] = matrix[top + i][right]
+            # move top left to top right
+            matrix[top + i][right] = topLeft
+            i += 1
+        right -= 1
+        left += 1
